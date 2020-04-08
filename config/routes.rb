f33ea082dc14e69c063a0a9bdd97856bb98e3809
Fanny Argoud <argoud.fanny@gmail.com>
2020-04-08 11:21:49 +0200

    Installed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: "centres#index"
   # Read all the centres (all)
   # Read one centre (all)
   # Create a centre (admin)
